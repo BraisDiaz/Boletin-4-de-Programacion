368bf49b204705a5d9c5ab8feb52a88084ecd7ef
Brais <Brais@Brais-PC>
2016-10-17 10:00:52 +0200

    Commit inicial del ejercicio 2-Boletin 4 (Circulo)

---
+++ Boletin4programacionejercicio2/src/boletin4programacionejercicio2/Boletin4programacionejercicio2.java
@@ -0,0 +1,18 @@
+
+package boletin4programacionejercicio2;
+
+/**
+ *
+ * @author Brais
+ */
+public class Boletin4programacionejercicio2 {
+
+    public static void main(String[] args) {
+       Satelite sat1 = new Satelite();
+   
+       sat1.verPosicion();
+       
+       
+    }
+    
+}
---
+++ Boletin4programacionejercicio2/src/boletin4programacionejercicio2/Satelite.java
@@ -0,0 +1,27 @@
+
+package boletin4programacionejercicio2;
+
+/**
+ *
+ * @author Brais
+ */
+public class Satelite {
+    
+    private double meridiano;
+    private double paralelo;
+    private double distanciaTerra;
+    
+public Satelite(){
+    meridiano = paralelo = distanciaTerra = 0;
+}
+public Satelite(double m, double p, double d){
+    meridiano = m;
+    paralelo = p;
+    distanciaTerra = d;
+}
+public void verPosicion(){
+    System.out.println("O satelite atopase  no paralelo " + paralelo + " meridiano " + meridiano + " a unha distancia da Terra " + distanciaTerra);
+   
+}
+
+}
