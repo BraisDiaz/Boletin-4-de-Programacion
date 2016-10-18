22b40c0710c3f17e3630e196d3231b3afc0d9aab
Brais <Brais@Brais-PC>
2016-10-18 09:36:07 +0200

    Segundo commit del ejercicio 4-Boletin 4 (Circulo) -AÑADIDO UTILIZAR MÉTODOS AÑADIR EN MAIN CLASS

--- Boletin4Programacionejercicio4/src/boletin4programacionejercicio4/Boletin4Programacionejercicio4.java
+++ Boletin4Programacionejercicio4/src/boletin4programacionejercicio4/Boletin4Programacionejercicio4.java
@@ -13,9 +13,13 @@ public class Boletin4Programacionejercicio4 {
     public static void main(String[] args) {
        
     Restaurante mesa1 = new Restaurante(2,1);
-    
+    //mostramos la cantidad de pulpo y patatas en el almacén y el nº de clientes que se pueden atender.
     mesa1.mostrarPulpo();
     mesa1.mostrarPatatas();
+    mesa1.mostrarClientes();
+    //añadimos pulpo y patatas al almacén y volvemos a mostrar el nº de clientes que se pueden atender.
+    mesa1.añadirPulpo(4);
+    mesa1.añadirPatatas(2);
     mesa1.mostrarClientes();
     }
     
